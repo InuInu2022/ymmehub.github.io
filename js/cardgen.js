b29d3934a0f620d60fc3fbfcf16859afa99d4744
RainKasa <hajiamotba@gmail.com>
2024-11-25 15:45:37 +0900

    New Design Update

--- js/cardgen.js
+++ js/cardgen.js
@@ -1,34 +1,33 @@
 console.log("loaded");
 $(window).on('load', function () {
-    $.getJSON("https://github.com/ymmehub/ymmehub.github.io/blob/main/json/plugins.json", (data) => {
-        for (let index = 0; index < data.plugins.length; index++) {
-            $("#grid").append(
-                `<div class="column is-half">
-                <a id="cell" href="/item.html?file=${data.plugins[index].id}">
-                    <div class="card is-fullheight">
-                        <div class="card-image">
-                            <figure class="image is-2by2 is-one-third">
-                                <img src="/file/img/${data.plugins[index].id}/1.png" alt="thunbnail"  />
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-content">
-                                    <p class="title is-4">${data.plugins[index].name}</p>
-                                    <p class="subtitle is-6">${data.plugins[index].author}</p>
-                                    <div class="content">
-                                        <p>${data.plugins[index].desc}</p>
+    $.getJSON("/json/plugins.json", (data) => {
+        data.map((value)=>{
+            $.getJSON(`/json/${value}.json`, (data) => {
+                $("#grid").append(
+                    `<div class="column is-half">
+                    <a id="cell" href="/item.html?file=${data.id}">
+                        <div class="card is-fullheight">
+                            <div class="card-image">
+                                <figure class="image is-2by2 is-one-third">
+                                    <img src="/file/img/${data.id}/1.png" alt="thunbnail"  />
+                                </figure>
+                            </div>
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-content">
+                                        <p class="title is-4">${data.name}</p>
+                                        <p class="subtitle is-6">${data.author}</p>
+                                        <div class="content">
+                                            <p>${data.desc}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </a>
                     </div>
-                </a>
-                </div>
-                `);
-                var resultFilter = data.plugins.filter(d => {
-                    return d.name === 'apple';
-                })
-        }
+                    `);
+            })
+        })
     });
 })
--- js/itemload.js
+++ js/itemload.js
@@ -9,54 +9,61 @@ function getParam(name, url) {
 }
 $(window).on('load', function () {
     const prm = getParam("file");
-        $.getJSON(`/json/${prm}.json`, (data) => {
-            // JSONデータを受信した後に実行する処理
-            document.title = `YMME Hub - ${data.name}`;
-            $("#title").text(data.name);
-            $("#author").text(data.name);
-            $("#description").html("<p>" + data.desc + "</p>");
-            const download = "/file/ymme/" + data.file;
-            $("#download").html('<a class="button is-success" href="' + "/file/ymme/" + data.file + '" download>ダウンロードする</a>');
-            console.log(data.img);
-            console.log(data.id);
-            console.log(download);
-            console.log(data.img);
-            console.log('/file/img/' + data.id + '/1.png');
-            switch (data.img) {
-                case "0":
-                    $("#img_item").html("<div></div>");
-                    break;
-                case "1":
-                    $("#img1").attr('src', '/file/img/' + data.id +'/1.png');
-                    for (let img = 2; img < 5; img++) {
-                        $("#img" + img).remove();
-                    }
-                    break;
-                case "2":
-                    for (let img = 1; img < 3; img++) {
-                        $("#img" + img).attr("src", "/file/img/" + data.id + "/" + img + ".png");
-                    }
-                    for (let img = 3; img < 5; img++) {
-                        $("#img" + img).remove();
-                    }
-                    break;
-                case "3":
-                    for (let img = 1; img < 4; img++) {
-                        $("#img" + img).attr("src", "/file/img/" + data.id + "/" + img + ".png");
-                    }
-                    for (let img = 4; img < 5; img++) {
-                        $("#img" + img).remove();
-                    }
-                    break;
-                case "4":
-                    for (let img = 1; img < 5; img++) {
-                        $("#img" + img).attr("src", "/file/img/" + data.id + "/" + img + ".png");
-                    }
-                    break;
-                default:
-                    break;
-            }
-        });
+    $.getJSON(`/json/plugins.json`, (data)=>{
+        if (data.indexOf(prm) != "-1") {
+            $.getJSON(`/json/${prm}.json`, (data) => {
+                // JSONデータを受信した後に実行する処理
+                document.title = `YMME Hub - ${data.name}`;
+                $("#title").text(data.name);
+                $("#author").text(data.name);
+                $("#description").html("<p>" + data.desc + "</p>");
+                const download = "/file/ymme/" + data.file;
+                $("#download").html('<a class="button is-success" href="' + "/file/ymme/" + data.file + '" download>ダウンロードする</a>');
+                console.log(data.img);
+                console.log(data.id);
+                console.log(download);
+                console.log(data.img);
+                console.log('/file/img/' + data.id + '/1.png');
+                switch (data.img) {
+                    case "0":
+                        $("#img_item").html("<div></div>");
+                        break;
+                    case "1":
+                        $("#img1").attr('src', '/file/img/' + data.id +'/1.png');
+                        for (let img = 2; img < 5; img++) {
+                            $("#img" + img).remove();
+                        }
+                        break;
+                    case "2":
+                        for (let img = 1; img < 3; img++) {
+                            $("#img" + img).attr("src", "/file/img/" + data.id + "/" + img + ".png");
+                        }
+                        for (let img = 3; img < 5; img++) {
+                            $("#img" + img).remove();
+                        }
+                        break;
+                    case "3":
+                        for (let img = 1; img < 4; img++) {
+                            $("#img" + img).attr("src", "/file/img/" + data.id + "/" + img + ".png");
+                        }
+                        for (let img = 4; img < 5; img++) {
+                            $("#img" + img).remove();
+                        }
+                        break;
+                    case "4":
+                        for (let img = 1; img < 5; img++) {
+                            $("#img" + img).attr("src", "/file/img/" + data.id + "/" + img + ".png");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            })
+        }else{
+            location.href = "/index.html"
+        }
+    })
+        
     $(".navbar-burger").click(function () {
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
